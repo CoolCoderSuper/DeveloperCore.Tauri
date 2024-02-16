19993b6d136a4dccc43b311d783b50e8e33d7fbf
Joseph Guenther <joseph@codingcool.ca>
2024-02-16 11:43:33 -0500

    Fix server navigation

--- DeveloperCore.Tauri.Local/LocalRunner.vb
+++ DeveloperCore.Tauri.Local/LocalRunner.vb
@@ -1,4 +1,5 @@
 Imports WatsonWebserver
+Imports WatsonWebserver.Core
 
 Public Class LocalRunner
     Implements IRunner
@@ -15,10 +16,26 @@ Public Class LocalRunner
 
     Public Sub Run() Implements IRunner.Run
         Host.Start()
-        Dim server As New Webserver(New Core.WebserverSettings("localhost", _port + 1), Async Function(x) Await x.Response.Send(""))
-        server.Routes.PreAuthentication.Content.Add("/", True)
+        Dim server As New Webserver(New WebserverSettings("localhost", _port + 1), Async Function(ctx)
+                                                                                       If ctx.Request.Method = HttpMethod.GET Then
+                                                                                           Dim route As String = ctx.Request.Url.RawWithQuery.Substring(1)
+                                                                                           If IO.File.Exists(route) Then
+                                                                                               Dim ext As String = IO.Path.GetExtension(route).ToLower()
+                                                                                               Dim content As Byte() = IO.File.ReadAllBytes(route)
+                                                                                               ctx.Response.StatusCode = 200
+                                                                                               ctx.Response.ContentType = MimeTypes.GetFromExtension(ext)
+                                                                                               Await ctx.Response.Send(content)
+                                                                                           Else
+                                                                                               Dim indexPath As String = "index.html"
+                                                                                               Dim content As String = IO.File.ReadAllText(indexPath)
+                                                                                               ctx.Response.StatusCode = 200
+                                                                                               ctx.Response.ContentType = "text/html"
+                                                                                               Await ctx.Response.Send(content)
+                                                                                           End If
+                                                                                       End If
+                                                                                   End Function)
         server.Start()
-        _webView.Run($"localhost:{_port + 1}/index.html")
+        _webView.Run($"localhost:{_port + 1}/")
         Host.Stop()
         server.Stop()
     End Sub
